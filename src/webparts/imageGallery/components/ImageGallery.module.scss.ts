fe2c10940ffac23924fa4f40eaef32c840deaaf3
Sumit Kanchan <sumit.kanchan04@live.com>
2017-08-11 14:42:14 +0200

    Changes made to Webpart

---
+++ src/webparts/imageGallery/components/ImageGallery.module.scss.ts
@@ -0,0 +1,15 @@
+/* tslint:disable */
+require('./ImageGallery.module.css');
+const styles = {
+  imageGallery: 'imageGallery_b0a88fda',
+  container: 'container_b0a88fda',
+  boxShadow: 'boxShadow_b0a88fda',
+  heading: 'heading_b0a88fda',
+  padding: 'padding_b0a88fda',
+  hide: 'hide_b0a88fda',
+  show: 'show_b0a88fda',
+  colBlack: 'colBlack_b0a88fda',
+};
+
+export default styles;
+/* tslint:enable */
